6e51ac3db2ae9430ba0468895c2d69b4b071b9fc
Stas Kirman <heft9nic@gmail.com>
2014-11-13 15:22:01 +0200

    Blog.1

---
+++ src/Acme/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Acme\BlogBundle\Controller;
+
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BlogController extends  Controller
+{
+    public function showAction($slug)
+    {
+        // use the $slug variable to query the database
+//        $blog = ...;
+
+        return $this->render('AcmeBlogBundle:Blog:show.html.twig', array(
+            'blog' => $slug,
+        ));
+    }
+    public function homepageAction($page)
+    {
+
+    }
+    public  function stasAction()
+    {
+        return $this->render('AcmeBlogBundle:Blog:stas.html.twig', array(
+            'blog' => 'stas',
+        ));
+    }
+}
