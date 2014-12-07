fe28db2150cefa72927504e368b890e5b38c35a7
Stas Kirman <heft9nic@gmail.com>
2014-12-07 16:09:36 +0200

    add bd rec

--- src/Acme/BlogBundle/Controller/DefaultController.php
+++ src/Acme/BlogBundle/Controller/DefaultController.php
@@ -3,11 +3,45 @@
 namespace Acme\BlogBundle\Controller;
 
 use Acme\BlogBundle\Entity\Car;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Acme\BlogBundle\Entity\Task;
+
 
 class DefaultController extends  Controller
 {
+    public function showAction(Request $request)
+
+    {
+        $task = new Task();
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', 'text')
+            ->add('dueDate', 'datetime')
+            ->add('save', 'submit', array('label' => 'Creat Task'))
+            ->add('saveAndAdd', 'submit', array('label' => 'Save and Add'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+
+          //  return $this->redirect($this->generateUrl('task_success'));
+        }
+
+
+        return $this->render('AcmeBlogBundle:Default:show.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+
+
     public function createAction()
     {
         $car = new Car();
@@ -22,18 +56,18 @@ class DefaultController extends  Controller
         return new Response('Created product id '.$car->getId());
     }
 
-    public function showAction($id)
+
+    public function updateAction($id)
     {
-        $car = $this->getDoctrine()
-            ->getRepository('AcmeBlogBundle:Car')
-            ->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $car = $em->getRepository('AcmeBlogBundle:Car')->find($id);
 
         if (!$car) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
+            throw $this->createNotFoundException('NO product  found for id'.$id);
         }
+    $car->setName('New product name!');
+    $em->flush();
 
-        // ... do something, like pass the $product object into a template
+        return $this->redirect($this->generateUrl1('show_page'));
     }
 }
---
+++ src/Acme/BlogBundle/Entity/Task.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Acme\BlogBundle\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="task")
+ */
+Class Task
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $task;
+
+    /**
+     * @Assert\Type("\DateTime")
+     * @ORM\Column(type="datetime", length=100)
+     */
+    protected  $dueDate;
+
+    public function getTask()
+    {
+        return $this->task;
+    }
+
+    public function setTask($task)
+    {
+        $this->task = $task;
+    }
+
+    public function  getDueDate()
+    {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(\DateTime $dueDate = null)
+    {
+        $this->dueDate = $dueDate;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+}
